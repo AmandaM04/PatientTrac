82a9c559c03c031b05d45e66d582d2adcb1fb79b
AmandaM04 <wreedamitchell@gmail.com>
2018-10-25 14:24:45 -0500

    made Type required

--- PatientTrac/Models/Medication.cs
+++ PatientTrac/Models/Medication.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,12 @@ namespace PatientTrac.Models
 {
     public class Medication
     {
+        [Key]
+        public int MedicationId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string Type { get; set; }
     }
 }
