5d6030036216221c05e868a46c1f006dd46e25f2
AmandaM04 <wreedamitchell@gmail.com>
2018-10-25 14:13:03 -0500

    created the patient model with data annotations

---
+++ PatientTrac/Models/Patient.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PatientTrac.Models
+{
+    public class Patient
+    {
+        [Key]
+        public int PatientId { get; set; }
+
+        [Required]
+        [Display(Name = "First name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last name")]
+        public string LastName { get; set; }
+
+        [Required]
+        public string StreetAddress { get; set; }
+
+        [Required]
+        public string PhoneNumber { get; set; }
+
+        public int Age { get; set; }
+
+        public string Sex { get; set; }
+
+        public string Conditions { get; set; }
+
+        [Display(Name = "Patient Name")]
+        public string FullName
+        {
+            get
+            {
+                return $"{FirstName} {LastName}";
+            }
+        }
+    }
+}
