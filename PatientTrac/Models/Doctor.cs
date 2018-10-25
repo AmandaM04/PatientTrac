8d6ac754031d851f00c797973624723768959937
AmandaM04 <wreedamitchell@gmail.com>
2018-10-25 14:03:22 -0500

    created project

---
+++ PatientTrac/Models/Doctor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PatientTrac.Models
+{
+    public class Doctor
+    {
+        [Key]
+        public int DoctorId { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        public string Facility { get; set; }
+    }
+}
