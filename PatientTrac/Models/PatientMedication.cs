63be3af4c467f15b8d6adbdf4842c27ef17b48d7
AmandaM04 <wreedamitchell@gmail.com>
2018-10-25 14:20:50 -0500

    added PatientMedication Join Table Model with data annotations

---
+++ PatientTrac/Models/PatientMedication.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PatientTrac.Models
+{
+    public class PatientMedication
+    {
+        [Key]
+        public int PatientMedicationId { get; set; }
+
+        [Required]
+        public bool CurrentlyTaking { get; set; }
+
+        [Required]
+        public DateTime StartDate { get; set; }
+
+        [Required]
+        public DateTime StopDate { get; set; }
+
+        [Required]
+        public int Dosage { get; set; }
+
+        [Required]
+        public int PatientId { get; set; }
+
+        public Patient Patient { get; set; }
+
+        [Required]
+        public int MedicationId { get; set; }
+
+        public Medication Medication { get; set; }
+    }
+}
