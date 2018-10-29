03df04492de36a5fab6801c9f8aa293204471668
AmandaM04 <wreedamitchell@gmail.com>
2018-10-29 10:19:11 -0500

    added Identity to the doctor

--- PatientTrac/Models/Doctor.cs
+++ PatientTrac/Models/Doctor.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace PatientTrac.Models
 {
-    public class Doctor
+    public class Doctor : IdentityUser
     {
         [Key]
         public int DoctorId { get; set; }
